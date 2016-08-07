a423c73a005520d146aaa997f195b0c826c42c09
Jory Hogeveen <jory.johan.hogeveen@gmail.com>
2016-08-07 21:08:31 +0200

    Add functionality to the global localized variable

--- js/off-canvas-sidebars.js
+++ js/off-canvas-sidebars.js
@@ -1,109 +1,120 @@
+;/**
+ * Off-Canvas Sidebars plugin
+ *
+ * OCS_OFF_CANVAS_SIDEBARS
+ * @author Jory Hogeveen <info@keraweb.nl>
+ * @package off-canvas-sidebars
+ * @version 0.2.0
+ */
 
-;(function($) {
+if ( typeof OCS_OFF_CANVAS_SIDEBARS == 'undefined' ) {
+	var OCS_OFF_CANVAS_SIDEBARS = {
+		"site_close":"",
+		"disable_over":"",
+		"hide_control_classes":"",
+		"scroll_lock":"",
+		"sidebars":{}
+	};
+}
 
-	var ocs_controller = {
+(function($) {
 
-		slidebars_controller: false,
-		site_close: OCS_OFF_CANVAS_SIDEBARS.site_close,
-		disable_over: OCS_OFF_CANVAS_SIDEBARS.disable_over,
-		hide_control_classes: OCS_OFF_CANVAS_SIDEBARS.hide_control_classes,
-		scroll_lock: OCS_OFF_CANVAS_SIDEBARS.scroll_lock,
+	OCS_OFF_CANVAS_SIDEBARS.slidebars_controller = false;
 
-		init: function() {
+	OCS_OFF_CANVAS_SIDEBARS.init = function() {
 
-			if ( false == ocs_controller.slidebars_controller ) {
-				return;
-			}
+		if ( false === OCS_OFF_CANVAS_SIDEBARS.slidebars_controller ) {
+			return;
+		}
 
-			var controller = ocs_controller.slidebars_controller;
-			controller.init();
+		var controller = OCS_OFF_CANVAS_SIDEBARS.slidebars_controller;
+		controller.init();
 
-			$( '.sb-slidebar' ).each( function(e) {
-				var id = $( this ).attr('off-canvas-sidebar-id');
-				$(document).on( 'click', '.sb-toggle-' + id, function(e) {
-					// Stop default action and bubbling
-  					e.stopPropagation();
-  					e.preventDefault();
+		$( '.sb-slidebar' ).each( function(e) {
+			var id = $( this ).attr('off-canvas-sidebar-id');
+			$(document).on( 'click', '.sb-toggle-' + id, function(e) {
+				// Stop default action and bubbling
+				e.stopPropagation();
+				e.preventDefault();
 
-  					// Toggle the slidebar with respect for the disable_over setting
-  					if ( ocs_controller.disable_over ) {
-						if ( $( window ).width() <= disable_over ) {
-		  					controller.toggle( 'sb-' + id );
-		  				}
-  					} else {
-  						controller.toggle( 'sb-' + id );
-  					}
-				} );
-			} );
-
-			// Close any
-			$( document ).on( 'click', '.sb-close-any', function( e ) {
-				if ( controller.getActiveSlidebar() ) {
-					e.preventDefault();
-					e.stopPropagation();
-					controller.close();
+				// Toggle the slidebar with respect for the disable_over setting
+				if ( OCS_OFF_CANVAS_SIDEBARS.disable_over ) {
+				if ( $( window ).width() <= disable_over ) {
+  					controller.toggle( 'sb-' + id );
+  				}
+				} else {
+					controller.toggle( 'sb-' + id );
 				}
 			} );
+		} );
 
-			// Close Slidebars when clicking on a link within a slidebar
-			$( '[off-canvas] a' ).on( 'click', function( e ) {
+		// Close any
+		$( document ).on( 'click', '.sb-close-any', function( e ) {
+			if ( controller.getActiveSlidebar() ) {
 				e.preventDefault();
 				e.stopPropagation();
+				controller.close();
+			}
+		} );
 
-				var url = $( this ).attr( 'href' ),
-				target = $( this ).attr( 'target' ) ? $( this ).attr( 'target' ) : '_self';
+		// Close Slidebars when clicking on a link within a slidebar
+		$( '[off-canvas] a' ).on( 'click', function( e ) {
+			e.preventDefault();
+			e.stopPropagation();
 
-				controller.close( function () {
-					window.open( url, target );
-				} );
+			var url = $( this ).attr( 'href' ),
+			target = $( this ).attr( 'target' ) ? $( this ).attr( 'target' ) : '_self';
+
+			controller.close( function () {
+				window.open( url, target );
+			} );
+		} );
+
+		if ( OCS_OFF_CANVAS_SIDEBARS.site_close ) {
+			// Add close class to canvas container when Slidebar is opened
+			$( controller.events ).on( 'opening', function ( e ) {
+				$( '[canvas]' ).addClass( 'sb-close-any' );
 			} );
 
-			if ( ocs_controller.site_close ) {
-				// Add close class to canvas container when Slidebar is opened
-				$( controller.events ).on( 'opening', function ( e ) {
-					$( '[canvas]' ).addClass( 'sb-close-any' );
-				} );
+			// Add close class to canvas container when Slidebar is opened
+			$( controller.events ).on( 'closing', function ( e ) {
+				$( '[canvas]' ).removeClass( 'sb-close-any' );
+			} );
+		}
 
-				// Add close class to canvas container when Slidebar is opened
-				$( controller.events ).on( 'closing', function ( e ) {
-					$( '[canvas]' ).removeClass( 'sb-close-any' );
-				} );
-			}
-
-			// Disable slidebars when the window is wider than the set width
-			if ( ocs_controller.disable_over ) {
-				var disableOver = function() {
-					if ( $( window ).width() > ocs_controller.disable_over ) {
-						controller.close();
-						// Hide control classes
-						if ( ocs_controller.hide_control_classes ) {
-							$( '.sb-toggle' ).hide();
-						}
-					} else {
-						$( '.sb-toggle' ).show();
+		// Disable slidebars when the window is wider than the set width
+		if ( OCS_OFF_CANVAS_SIDEBARS.disable_over ) {
+			var disableOver = function() {
+				if ( $( window ).width() > OCS_OFF_CANVAS_SIDEBARS.disable_over ) {
+					controller.close();
+					// Hide control classes
+					if ( OCS_OFF_CANVAS_SIDEBARS.hide_control_classes ) {
+						$( '.sb-toggle' ).hide();
 					}
-				};
-				disableOver();
-				$( window ).on( 'resize', disableOver );
+				} else {
+					$( '.sb-toggle' ).show();
+				}
+			};
+			disableOver();
+			$( window ).on( 'resize', disableOver );
 
-			}
+		}
 
-			if ( ocs_controller.scroll_lock ) {
-				$('#sb-site').on( 'scroll touchmove mousewheel', function( e ) {
-					if ( false != controller.getActiveSlidebar() ) {
-						e.preventDefault();
-						e.stopPropagation();
-						return false;
-					}
-				} );
-			}
+		if ( OCS_OFF_CANVAS_SIDEBARS.scroll_lock ) {
+			$('#sb-site').on( 'scroll touchmove mousewheel', function( e ) {
+				if ( false != controller.getActiveSlidebar() ) {
+					e.preventDefault();
+					e.stopPropagation();
+					return false;
+				}
+			} );
+		}
 
-		},
 	};
 
 	if ( $('#sb-site').length > 0 && ( typeof slidebars != 'undefined' ) ) {
-		ocs_controller.slidebars_controller = new slidebars();
-		ocs_controller.init();
+		OCS_OFF_CANVAS_SIDEBARS.slidebars_controller = new slidebars();
+		OCS_OFF_CANVAS_SIDEBARS.init();
 	}
 
 }) (jQuery);
