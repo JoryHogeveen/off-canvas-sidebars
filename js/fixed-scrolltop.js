7f4762cdbfe52184c9ca99fc3f6adcbc497f1536
Jory Hogeveen <jory.johan.hogeveen@gmail.com>
2016-10-06 19:14:14 +0200

    Improve the target and positioning mechanism for scrollTop()

--- js/fixed-scrolltop.js
+++ js/fixed-scrolltop.js
@@ -16,43 +16,49 @@
 		}
 
 		var curScrollTopElements;
-		var ocs_site = $('#' + prefix + '-site');
+		var scrollTarget = ocs_site = $('#' + prefix + '-site');
+		if ( 'auto' != ocs_site.css('overflow-y') ) {
+			scrollTarget = $( window );
+		}
 
 		if ( ocs_site.css('transform') != 'none' ) {
 			curScrollTopElements = $('#' + prefix + '-site *').filter( function(){ return $(this).css('position') === 'fixed'; } );
 			ocsScrollTopFixed();
-			ocs_site.on('scroll resize', function() {
+			scrollTarget.on('scroll resize', function() {
 				var newScrollTopElements = $('#' + prefix + '-site *').filter( function(){ return $(this).css('position') === 'fixed'; } );
-				curScrollTopElements = curScrollTopElements.add(newScrollTopElements);
+				curScrollTopElements = curScrollTopElements.add( newScrollTopElements );
 				ocsScrollTopFixed();
-			});
+			} );
 		}
 
 		function ocsScrollTopFixed() {
 			if ( curScrollTopElements.length > 0 ) {
-				var scrollTop = ocs_site.scrollTop();
-				var winHeight = $(window).height();
-				var conOffset = ocs_site.offset();
-				var conHeight = ocs_site.outerHeight();
+				var scrollTop = scrollTarget.scrollTop(),
+				    winHeight = $(window).height(),
+				    gblOffset = $('body').offset(),
+				    conOffset = ocs_site.offset(),
+				    conHeight = ocs_site.outerHeight();
 				curScrollTopElements.each( function() {
 					if ( $(this).css('position') == 'fixed' ) {
-						var top = $(this).css('top');
-						var bottom = $(this).css('bottom');
-						var px;
+						var top = $(this).css('top'),
+						    bottom = $(this).css('bottom'),
+							px;
 						if ( top == 'auto' && bottom != 'auto' ) {
-							px = ( scrollTop + winHeight ) - ( conOffset.top + conHeight );
+							px = ( scrollTop + winHeight ) - ( conOffset.top + conHeight ) + gblOffset.top;
 						} else {
-							px = scrollTop - conOffset.top;
+							px = scrollTop - conOffset.top + gblOffset.top;
 						}
 						$(this).css({
-							'-webkit-transform': 'translateY(' + px + 'px)',
-							'-moz-transform': 'translateY(' + px + 'px)',
-							'transform': 'translateY(' + px + 'px)'
+							'-webkit-transform': 'translate( 0px, ' + px + 'px )',
+							'-moz-transform': 'translate( 0px, ' + px + 'px )',
+							'-o-transform': 'translate( 0px, ' + px + 'px )',
+							'transform': 'translate( 0px, ' + px + 'px )'
 						});
 					} else {
 						$(this).css({
 							'-webkit-transform' : '',
 							'-moz-transform' : '',
+							'-o-transform' : '',
 							'transform' : ''
 						});
 					}
