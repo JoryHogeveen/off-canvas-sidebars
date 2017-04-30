e07df9d017ec7f41686c8e0909dc77bf81571139
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2017-04-30 20:14:51 +0200

    Fix PHP unit tests

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -1,9 +1,15 @@
 <?php
+/**
+ * Off-Canvas Sidebars - Unit tests
+ *
+ * @author  Jory Hogeveen <info@keraweb.nl>
+ * @package Off_Canvas_Sidebars
+ */
 
-class PluginTest extends WP_UnitTestCase {
+class OCS_UnitTest extends WP_UnitTestCase {
 
-  // Check that that activation doesn't break
-  function test_plugin_activated() {
-    $this->assertTrue( is_plugin_active( PLUGIN_PATH ) );
-  }
+	// Check that that activation doesn't break
+	function test_plugin_activated() {
+		$this->assertTrue( is_plugin_active( TEST_OCS_PLUGIN_PATH ) );
+	}
 }
