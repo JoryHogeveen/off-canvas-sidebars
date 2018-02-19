f93d919c66c4b3fbea4e1f57952b56d873f255e4
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2018-02-19 23:58:42 +0100

    Codestyle, phpdoc, etc.

--- includes/class-ocs.php
+++ includes/class-ocs.php
@@ -138,12 +138,12 @@ final class OCS_Off_Canvas_Sidebars
 	}
 
 	/**
-	 * Init function/action to check current user, load nessesary data and classes, register hooks.
+	 * Init function/action to check current user, load necessary data and classes, register hooks.
 	 *
 	 * @return  void
 	 * @since   0.1
 	 */
-	function init() {
+	public function init() {
 		// Get the current user.
 		//$this->curUser = wp_get_current_user();
 
@@ -179,7 +179,7 @@ final class OCS_Off_Canvas_Sidebars
 	 * Register widgets.
 	 * @since  0.4
 	 */
-	function widgets_init() {
+	public function widgets_init() {
 		register_widget( 'OCS_Off_Canvas_Sidebars_Control_Widget' );
 	}
 
@@ -190,12 +190,12 @@ final class OCS_Off_Canvas_Sidebars
 	 * @return  void
 	 * @since   0.1
 	 */
-	function compatibility_notice() {
+	public function compatibility_notice() {
 		if ( get_user_meta( $this->curUser->ID, $this->noticeKey, true ) !== $this->version ) {
 			$class = 'error notice is-dismissible';
 			$message = '<strong>' . __( 'Off-Canvas Sidebars', 'off-canvas-sidebars' ) . ':</strong> ' . $this->general_labels['compatibility_notice_theme'];
 			$ignore = '<a id="' . $this->noticeKey . '" href="?' . $this->noticeKey . '=1" class="notice-dismiss"><span class="screen-reader-text">' . __( 'Dismiss this notice.', OCS_DOMAIN ) . '</span></a>';
-			$script = '<script>(function($) { $(document).on("click", "#' . $this->noticeKey . '", function(e){e.preventDefault();$.post(ajaxurl, {\'action\': \'' . $this->noticeKey . '\'});}) })( jQuery );</script>';
+			$script = '<script>(function($) { $(document).on("click", "#' . $this->noticeKey . '", function(e) {e.preventDefault();$.post(ajaxurl, {\'action\': \'' . $this->noticeKey . '\'});}) })( jQuery );</script>';
 			echo '<div id="' . $this->noticeKey . '" class="' . $class . '"> <p>' . $message . '</p> ' . $ignore . $script . '</div>';
 		}
 	}
@@ -208,7 +208,7 @@ final class OCS_Off_Canvas_Sidebars
 	 *
 	 * @since   0.1
 	 */
-	function ignore_compatibility_notice() {
+	public function ignore_compatibility_notice() {
 		update_user_meta( $this->curUser->ID, $this->noticeKey, $this->version );
 		wp_die();
 	}
@@ -218,7 +218,7 @@ final class OCS_Off_Canvas_Sidebars
 	 * @param   string  $key
 	 * @return  mixed
 	 */
-	function get_settings( $key = null ) {
+	public function get_settings( $key = null ) {
 		return OCS_Off_Canvas_Sidebars_Settings::get_instance()->get_settings( $key );
 	}
 
@@ -227,7 +227,7 @@ final class OCS_Off_Canvas_Sidebars
 	 * @param   string  $id
 	 * @return  array|null
 	 */
-	function get_sidebars( $id = null ) {
+	public function get_sidebars( $id = null ) {
 		$sidebars = $this->get_settings( 'sidebars' );
 		if ( $id ) {
 			return ( isset( $sidebars[ $id ] ) ) ? $sidebars[ $id ] : null;
@@ -241,7 +241,7 @@ final class OCS_Off_Canvas_Sidebars
 	 * @since   0.1.2
 	 * @return  string
 	 */
-	function get_version() {
+	public function get_version() {
 		return $this->version;
 	}
 
@@ -261,7 +261,7 @@ final class OCS_Off_Canvas_Sidebars
 	 * @since   0.1
 	 * @return  string
 	 */
-	function get_plugin_key() {
+	public function get_plugin_key() {
 		return $this->plugin_key;
 	}
 
@@ -271,7 +271,7 @@ final class OCS_Off_Canvas_Sidebars
 	 * @since   0.1
 	 * @return  string
 	 */
-	function get_general_key() {
+	public function get_general_key() {
 		return $this->general_key;
 	}
 
@@ -283,7 +283,7 @@ final class OCS_Off_Canvas_Sidebars
 	 * @param   string  $key  (optional) The label key.
 	 * @return  array|string
 	 */
-	function get_general_labels( $key = null ) {
+	public function get_general_labels( $key = null ) {
 		static $labels;
 		if ( ! $labels ) {
 			$labels = array(
@@ -305,30 +305,33 @@ final class OCS_Off_Canvas_Sidebars
 	 * @param   string  $label
 	 * @return  string  $key
 	 */
-	function get_sidebar_key_by_label( $label ) {
+	public function get_sidebar_key_by_label( $label ) {
 		foreach ( $this->get_sidebars() as $key => $value ) {
-			if ( $label === $value['label'] )
+			if ( $label === $value['label'] ) {
 				return $key;
+			}
 		}
 		return false;
 	}
 
 	/**
-	 * Checks if a sidebar is enabled.
+	 * Checks if an off-canvas sidebar is enabled.
+	 * If no $id parameter is passed it will check if any off-canvas sidebar is enabled.
 	 *
 	 * @since   0.1
 	 * @since   0.5  Optional ID parameter
 	 * @param   string  $id  Sidebar ID.
 	 * @return  bool
 	 */
-	function is_sidebar_enabled( $id = null ) {
+	public function is_sidebar_enabled( $id = null ) {
 		$sidebars = $this->get_sidebars();
 		if ( $id ) {
 			return ( ! empty( $sidebars[ $id ]['enable'] ) );
 		}
 		foreach ( $this->get_sidebars() as $key => $value ) {
-			if ( ! empty( $value['enable'] ) )
+			if ( ! empty( $value['enable'] ) ) {
 				return true;
+			}
 		}
 		return false;
 	}
@@ -340,7 +343,7 @@ final class OCS_Off_Canvas_Sidebars
 	 * @since   0.1
 	 * @return  void
 	 */
-	function register_sidebars() {
+	public function register_sidebars() {
 		$sidebars = $this->get_sidebars();
 		foreach ( $sidebars as $sidebar_id => $sidebar_data ) {
 
@@ -404,7 +407,7 @@ final class OCS_Off_Canvas_Sidebars
 	 * @param   string  $file
 	 * @return  array
 	 */
-	function add_settings_link( $links, $file ) {
+	public function add_settings_link( $links, $file ) {
 		if ( OCS_BASENAME === $file ) {
 			$settings_link = '<a href="' . admin_url( 'themes.php?page=' . $this->plugin_key ) . '">' . esc_attr__( 'Settings', OCS_DOMAIN ) . '</a>';
 			array_unshift( $links, $settings_link );
@@ -418,12 +421,16 @@ final class OCS_Off_Canvas_Sidebars
 	 * @since   0.1
 	 * @return  void
 	 */
-	function load_textdomain() {
+	public function load_textdomain() {
 		load_plugin_textdomain( 'off-canvas-sidebars', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 	}
 
 	/**
 	 * Update settings.
+	 *
+	 * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+	 * @SuppressWarnings(PHPMD.NPathComplexity)
+	 * @todo Refactor to enable above checks?
 	 *
 	 * @since   0.2
 	 * @access  private
