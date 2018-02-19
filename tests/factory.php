90627536d4efc1b836ce7b081915b2982182c448
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2018-02-20 00:33:17 +0100

    Add array replace for PHP 5.2 tests

--- tests/factory.php
+++ tests/factory.php
@@ -87,3 +87,28 @@ class OCS_UnitTest_Factory {
 		return self::$_instance;
 	}
 }
+
+if ( ! function_exists( 'array_replace' ) ) {
+	function array_replace( array &$array, array &$array1 ) {
+		$args = func_get_args();
+		$count = func_num_args();
+
+		for ( $i = 0; $i < $count; ++$i ) {
+			if ( is_array( $args[ $i ] ) ) {
+				foreach ( $args[ $i ] as $key => $val ) {
+					$array[ $key ] = $val;
+				}
+			}
+			else {
+				trigger_error(
+					__FUNCTION__ . '(): Argument #' . ( $i + 1 ) . ' is not an array',
+					E_USER_WARNING
+				);
+				return NULL;
+			}
+		}
+
+		return $array;
+	}
+}
+
