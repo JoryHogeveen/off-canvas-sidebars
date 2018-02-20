8f279394a59a60305701251273c3480542aef98d
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2018-02-20 16:36:21 +0100

    Add sidebars tests

--- tests/test-settings.php
+++ tests/test-settings.php
@@ -101,5 +101,122 @@ class OCS_Settings_UnitTest extends WP_UnitTestCase {
 		// @todo Check compare with current (non-default) settings.
 		// @todo Change sidebar enabled. Use factory to set default sidebars?
 		// @todo More validation types.
+
+		/**
+		 * SIDEBARS
+		 * No new function because filters are reset.
+		 *
+		 * @fixme
+		 */
+
+		self::ocs_set_tab( 'ocs-sidebars' );
+		$settings = OCS_UnitTest_Factory::$settings;
+
+		$defaults = $settings->get_default_settings();
+		$defaults['db_version'] = off_canvas_sidebars()->get_db_version();
+
+		$compare = $defaults;
+		$compare['sidebars'] = array(
+			'new' => $settings->get_default_sidebar_settings(),
+		);
+
+		$new = array(
+			'sidebars' => array(
+				'new' => array(),
+			),
+		);
+
+		$this->assertEquals( $compare, $settings->validate_form( $new ) );
+
+		/**
+		 * Valid data.
+		 */
+
+		$new = array(
+			'sidebars' => array(
+				'test' => array(
+					// All valid, non default values.
+					'enable'                    => 1,
+					'label'                     => 'Test',
+					'content'                   => 'menu',
+					'location'                  => 'right',
+					'style'                     => 'overlay',
+					'size'                      => 'large',
+					'size_input'                => '300',
+					'size_input_type'           => 'px',
+					'animation_speed'           => '5000',
+					'padding'                   => '25',
+					'background_color'          => '#123456',
+					'background_color_type'     => 'color',
+					// Global overwrites.
+					'overwrite_global_settings' => 1,
+					'site_close'                => 0,
+					'link_close'                => 0,
+					'disable_over'              => '500',
+					'hide_control_classes'      => 1,
+					'scroll_lock'               => 1,
+				),
+			),
+		);
+
+		$compare['sidebars'] = $new['sidebars'];
+
+		$this->assertEquals( $compare, $settings->validate_form( $new ) );
+
+		/**
+		 * Invalid data.
+		 */
+
+		$new = array(
+			'sidebars' => array(
+				'test' => array(
+					// All invalid values.
+					'enable'                    => 'yes', // Parse as true (1)
+					'label'                     => 'Test <span>strip_tags test</span>',
+					'content'                   => array( 'action' ),// Invalid radio option, will return to default
+					'location'                  => 'nope', // Invalid radio option, will return to default
+					'style'                     => false, // Invalid radio option, will return to default
+					'size'                      => 'nope', // Invalid radio option, will return to default
+					'size_input'                => true, // Invalid numeric value, will return empty
+					'size_input_type'           => array(), // Invalid radio option, will return to default
+					'animation_speed'           => -5000, // Negative number should be parsed as positive
+					'padding'                   => 50.30, // Should be parsed as int string
+					'background_color'          => '123', // Should be parsed as `#123`
+					'background_color_type'     => false, // Invalid radio option, will return to default
+					// Global overwrites.
+					'overwrite_global_settings' => '', // Parse as false (0)
+					'site_close'                => '505', // Parse as true (1)
+					'link_close'                => 600, // Parse as true (1)
+					'disable_over'              => '500.60', // Parse as int string
+					'hide_control_classes'      => true, // Parse as true (1)
+					'scroll_lock'               => false, // Parse as false (0)
+				),
+			),
+		);
+
+		$compare['sidebars']['test'] = array(
+			// All parsed values.
+			'enable'                    => 1,
+			'label'                     => 'Test strip_tags test',
+			'content'                   => 'sidebar',
+			'location'                  => 'left',
+			'style'                     => 'push',
+			'size'                      => 'default',
+			'size_input'                => '',
+			'size_input_type'           => '%',
+			'animation_speed'           => '5000',
+			'padding'                   => '50',
+			'background_color'          => '#123',
+			'background_color_type'     => '',
+			// Global overwrites.
+			'overwrite_global_settings' => 0,
+			'site_close'                => 1,
+			'link_close'                => 1,
+			'disable_over'              => '500',
+			'hide_control_classes'      => 1,
+			'scroll_lock'               => 0,
+		);
+
+		$this->assertEquals( $compare, $settings->validate_form( $new ) );
 	}
 }
