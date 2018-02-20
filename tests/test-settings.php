0687c660a5881e1a7dde8001398cb1c830ec1154
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2018-02-20 16:36:04 +0100

    Enhance test formats

--- tests/test-settings.php
+++ tests/test-settings.php
@@ -18,16 +18,27 @@ class OCS_Settings_UnitTest extends WP_UnitTestCase {
 		self::ocs_set_tab( 'ocs-settings' );
 		$settings = OCS_UnitTest_Factory::$settings;
 
-		$defaults = $settings->get_default_settings();
+		$defaults               = $settings->get_default_settings();
 		$defaults['db_version'] = off_canvas_sidebars()->get_db_version();
 
-		// Empty data.
-		$this->assertEquals( $defaults, $settings->validate_form( array(
-			// These fields are on by default so will generate differences on save (checkboxes).
-			'enable_frontend' => 1,
-			'site_close' => 1,
-			'link_close' => 1,
-		) ) );
+		/**
+		 * Empty data.
+		 */
+		$this->assertEquals(
+			$defaults,
+			$settings->validate_form(
+				array(
+					// These fields are on by default so will generate differences on save (checkboxes).
+					'enable_frontend' => 1,
+					'site_close'      => 1,
+					'link_close'      => 1,
+				)
+			)
+		);
+
+		/**
+		 * Valid data.
+		 */
 
 		$new = array(
 			'enable_frontend'               => 0,
@@ -49,33 +60,40 @@ class OCS_Settings_UnitTest extends WP_UnitTestCase {
 		// All valid values.
 		$this->assertEquals( $compare, $settings->validate_form( $new ) );
 
+		/**
+		 * Invalid data.
+		 */
+
 		$new = array(
 			// Invalid values.
-			'enable_frontend'               => new stdClass(), // Parse as true (1)
-			'site_close'                    => 123, // Parse as true (1)
-			'link_close'                    => 321, // Parse as true (1)
-			'disable_over'                  => '300.123', // Parsed as integer
-			'background_color_type'         => 'color-123', // Invalid radio option, will return to default
-			'background_color'              => '#123456123', // Invalid color, will be trimmed
-			'website_before_hook'           => array(), // Invalid value, will return empty
-			'website_after_hook'            => array(), // Invalid value, will return empty
-			'use_fastclick'                 => true, // Parse as int version (1)
-			'compatibility_position_fixed'  => 'legacy-css-yay', // Invalid radio option, will return to default
+			'enable_frontend'              => new stdClass(), // Parse as true (1)
+			'site_close'                   => 123, // Parse as true (1)
+			'link_close'                   => 321, // Parse as true (1)
+			'disable_over'                 => '300.123', // Parsed as integer
+			'background_color_type'        => 'color-123', // Invalid radio option, will return to default
+			'background_color'             => '#123456123', // Invalid color, will be trimmed
+			'website_before_hook'          => array(), // Invalid value, will return empty
+			'website_after_hook'           => array(), // Invalid value, will return empty
+			'use_fastclick'                => true, // Parse as int version (1)
+			'compatibility_position_fixed' => 'legacy-css-yay', // Invalid radio option, will return to default
 		);
 
-		$compare = array_replace( $defaults, $new, array(
-			// Parsed values.
-			'enable_frontend'               => 1,
-			'site_close'                    => 1,
-			'link_close'                    => 1,
-			'disable_over'                  => '300',
-			'background_color_type'         => '',
-			'background_color'              => '#123456',
-			'website_before_hook'           => '',
-			'website_after_hook'            => '',
-			'use_fastclick'                 => 1,
-			'compatibility_position_fixed'  => 'none',
-		) );
+		$compare = array_replace(
+			$defaults,
+			array(
+				// Parsed values.
+				'enable_frontend'              => 1,
+				'site_close'                   => 1,
+				'link_close'                   => 1,
+				'disable_over'                 => '300',
+				'background_color_type'        => '',
+				'background_color'             => '#123456',
+				'website_before_hook'          => '',
+				'website_after_hook'           => '',
+				'use_fastclick'                => 1,
+				'compatibility_position_fixed' => 'none',
+			)
+		);
 
 		// All values should be parsed correctly.
 		$this->assertEquals( $compare, $settings->validate_form( $new ) );
