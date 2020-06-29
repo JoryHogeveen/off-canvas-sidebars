c1101302f76c6a99e0ff1a6747d77029c7e17040
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2020-06-29 17:03:19 +0200

    Start on setup-validation class

---
+++ includes/class-setup.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Off-Canvas Sidebars - Class Setup
+ *
+ * @author  Jory Hogeveen <info@keraweb.nl>
+ * @package Off_Canvas_Sidebars
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die();
+}
+
+/**
+ * Off-Canvas Sidebars plugin setup
+ *
+ * @author  Jory Hogeveen <info@keraweb.nl>
+ * @package Off_Canvas_Sidebars
+ * @since   0.5.6
+ * @version 0.5.6
+ * @uses    \OCS_Off_Canvas_Sidebars_Base Extends class
+ */
+final class OCS_Off_Canvas_Sidebars_Setup extends OCS_Off_Canvas_Sidebars_Base
+{
+	/**
+	 * The single instance of the class.
+	 *
+	 * @var    \OCS_Off_Canvas_Sidebars_Setup
+	 * @since  0.5.6
+	 */
+	protected static $_instance = null;
+
+	/**
+	 * Class constructor.
+	 *
+	 * @since   0.5.6
+	 * @access  private
+	 */
+	private function __construct() {
+		if ( ! OCS_Off_Canvas_Sidebars_Page::get_instance()->has_access() ) {
+			return;
+		}
+		if ( isset( $_GET['ocs-setup-validate'] ) ) {
+			$this->run_validation();
+		}
+	}
+
+	/**
+	 * Add validation hooks.
+	 */
+	public function run_validation() {
+	}
+
+	/**
+	 * Class Instance.
+	 * Ensures only one instance of this class is loaded or can be loaded.
+	 *
+	 * @since   0.5.6
+	 * @static
+	 * @return  \OCS_Off_Canvas_Sidebars_Setup
+	 */
+	public static function get_instance() {
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+
+} // End class().
